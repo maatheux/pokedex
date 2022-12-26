84b9004b8bc214b0d3d70871cc8ae37e9b0326cf
Matheus Lima <matheusmarinho.lima@hotmail.com>
2022-12-26 18:49:24 -0300

    Start search feature building

--- src/app/shared/poke-search/poke-search.component.ts
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-poke-search',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeSearchComponent implements OnInit {
 
+  @Output() public emitSearch: EventEmitter<string> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public searchPokemon(value: string) {
+    this.emitSearch.emit(value);
   }
 
 }
